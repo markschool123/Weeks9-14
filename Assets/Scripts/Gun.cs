7b928f3c4077b7b9b29657c90fb0b2f66e1ff617
Mark Fernandes <ferna100@shernet.sheridancollege.ca>
2026-03-24 13:04:04 -0400

    created coroutine

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,13 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-  
+    public int ammo;
+    public int maxAmmo;     
+    public UnityEvent OnFire;
+    public bool isReloading;
+    public float reloadTime = 3;
     void Start()
     {
-        
+        ammo = maxAmmo;
     }
 
     // Update is called once per frame
@@ -15,4 +21,34 @@ public class Gun : MonoBehaviour
     {
         
     }
+
+    public void Shoot()
+    {
+        ammo--;
+        OnFire.Invoke();
+
+        if (ammo==0)
+        {
+            StartCoroutine(Reloading());
+        }
+    }
+
+    IEnumerator Reloading()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+       
+        isReloading=false;
+        ammo = maxAmmo;
+    }
+
+    
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+
+    }
+
+
+
+
 }
